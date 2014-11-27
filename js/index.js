a2db2cdb8d10066f6edad609dcfed8673ad5c6b8
ellenlai <miss.ellenlai@gmail.com>
2014-11-27 12:42:01 -0500

    Fix offset issue

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 
 $(document).ready(function() {
   $('.nav_item').hide();
-var stickyNavTop = $('.nav').offset().top;
+var stickyNavTop = $('.nav').offset().top -500;
 
 var stickyNav = function(){
 var scrollTop = $(window).scrollTop();
 
 
 if (scrollTop > stickyNavTop) {
-    $('.nav').addClass('sticky');
     $('.nav_item').show();
+    $('.nav').addClass('sticky');
 } else {
     $('.nav').removeClass('sticky');
     $('.nav_item').hide();
