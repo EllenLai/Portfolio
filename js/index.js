2d78b5f6b2d75cbfefdd8be80721e565d0ef489f
ellenlai <miss.ellenlai@gmail.com>
2014-11-30 17:53:13 -0500

    Fix container issues

--- js/index.js
+++ js/index.js
@@ -72,7 +72,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 80
+          scrollTop: target.offset().top - 50
         }, 1000);
         return false;
       }
